1a36106d465608142bdb41bc99316e05a2950639
septor <patrickweaver@gmail.com>
2016-03-30 12:32:18 -0400

    getEvents!

--- _class.php
+++ _class.php
@@ -19,7 +19,44 @@ class Calevents
 		$this->client->setApplicationName($this->pref['calendar_name'];
 		$this->client->setDeveloperKey($this->pref['develper_api'];
 		$this->calendar = new Google_Service_Calendar($this->client);
+
+		$this->params = array(
+			'singleEvents' => $this->pref['singleEvents'],
+			'orderBy' => $this->pref['event_orderby'],
+			'maxResults' => $this->pref['max_results']
+		);
 	}
 
-	function 
+	function getEvents($calendarId)
+	{
+		$events = $this->calendar->events->listEvents($calendarId, $this->params);
+		$calTimeZone = $events->timeZone;
+
+		date_default_timezone_set($events->timeZone);
+
+		foreach($events->getItems() as $event)
+		{
+			$eventDateStr = $event->start->dateTime;
+			if(empty($eventDateStr))
+				$eventDateStr = $event->start->date;
+
+			$tmpTz = $event->start->timeZone;
+			if(!empty($tmpTz))
+				$timezone = new DateTimeZone($tmpTz);
+			else
+				$timezone = new DateTimeZone($calTimeZone);
+
+			$eventDate = new DateTime($eventDateStr, $timezone);
+
+			$output[] = array(
+				'link' => $event->htmllink,
+				'timezone_link' => $event->htmllink.'&ctz='$calTimeZone,
+				'month' => $eventDate->format('M'),
+				'day' => $eventDate->format('j'),
+				'summary' => $event->summary
+			);
+		}
+
+		return $output;
+	}
 }
